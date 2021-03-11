8d677b620a0037f2ce711ee0a71e72127e553c9d
Mason Halstead <>
2021-03-10 17:48:18 -0800

    fixing accordion table

--- src/tables/TableAccordion.js
+++ src/tables/TableAccordion.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { TableBase } from './TableBase'
+import { RowsElement } from './elements'
+import { Rows } from './Rows'
+import cn from './Table.module.scss'
 import { RowAccordion } from './RowAccordion'
 
 export const TableAccordion = ({
@@ -20,14 +23,16 @@ export const TableAccordion = ({
     uuid: -1
   }
   return (
-    <TableBase
-      {...rest}
-      settings={{ ...settings, accordion: true }}
-      headers={[...headers, header]}
-    >
-      <RowAccordion accordion={accordion} transition={transition}>
+    <TableBase {...rest} headers={[...headers, header]}>
+      <TableWrapper
+        accordion={accordion}
+        accordion_padding={accordion_padding}
+        expanded_icon={expanded_icon}
+        expand_icon={expand_icon}
+        transition={transition}
+      >
         {children}
-      </RowAccordion>
+      </TableWrapper>
     </TableBase>
   )
 }
@@ -49,5 +54,78 @@ TableAccordion.propTypes = {
   accordion_padding: PropTypes.string,
   expanded_icon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   expand_icon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
+}
+
+const TableWrapper = ({
+  accordion,
+  accordion_padding,
+  expanded_icon,
+  expand_icon,
+  transition,
+  settings,
+  headers,
+  rows,
+  sorting,
+  theme,
+  children
+}) => {
+  const { headers_show, row_borders } = settings
+  return (
+    <RowsElement
+      className={cn.rows}
+      borders={row_borders}
+      theme={theme}
+      style={{
+        borderTop: !headers_show && `1px solid ${theme.border_table_color}`
+      }}
+    >
+      <Rows
+        headers={headers}
+        sorting={sorting}
+        theme={theme}
+        rows={rows}
+        settings={settings}
+      >
+        <RowAccordion
+          accordion={accordion}
+          accordion_padding={accordion_padding}
+          expanded_icon={expanded_icon}
+          expand_icon={expand_icon}
+          transition={transition}
+        >
+          {children}
+        </RowAccordion>
+      </Rows>
+    </RowsElement>
+  )
+}
+
+TableWrapper.defaultProps = {
+  settings: {},
+  headers: [],
+  rows: [],
+  sorting: {},
+  expanded: false,
+  accordion: () => {},
+  accordion_padding: '20px',
+  expanded_icon: ['fas', 'chevron-down'],
+  expand_icon: ['fas', 'chevron-right'],
+  transition: 600,
+  theme: {},
+  children: () => {}
+}
+TableWrapper.propTypes = {
+  settings: PropTypes.object,
+  headers: PropTypes.array,
+  rows: PropTypes.array,
+  sorting: PropTypes.object,
+  theme: PropTypes.object,
+  accordion_padding: PropTypes.string,
+  transition: PropTypes.number,
+  accordion: PropTypes.any,
+  expanded_icon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  expand_icon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  expanded: PropTypes.bool,
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
 }
